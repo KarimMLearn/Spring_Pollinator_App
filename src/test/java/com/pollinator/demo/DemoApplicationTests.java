aad49b73c412f1a5125c88297c1aab5ea72ae176
Karim <karimharoun@gmail.com>
2024-06-12 12:33:34 +0300

    Initial commit of code

---
+++ src/test/java/com/pollinator/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollinator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
